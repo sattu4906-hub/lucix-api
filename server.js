f5aef9dc6da7ae0913ea2a00387ddc235649b37d
sattu4906-hub <sattu4906@gmail.com>
2025-11-22 20:24:21 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,19 +1,13 @@
 import express from "express";
-import cors from "cors";
-
 const app = express();
-app.use(cors());
-app.use(express.json());
 
-// Test Route
 app.get("/", (req, res) => {
-  res.send("Lucix API is running 🚀");
+  res.send("Lucix API running on Railway 🚀");
 });
 
-// Render port requirement
+// Required by Railway
 const port = process.env.PORT || 3000;
 
 app.listen(port, "0.0.0.0", () => {
   console.log(`Lucix API running on port ${port}`);
 });
-
